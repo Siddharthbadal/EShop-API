c309e5d9bdd39b236b383447354179abae8ab891
siddharthbadal <siddharthabadal.2015@gmail.com>
2023-04-24 22:45:19 +0530

    added new endpoints

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('myaccount/', views.get_current_user, name='get_current_user'),
     path('myaccount/update', views.update_user_info, name='update_user_info'),
     path('myaccount/update_password', views.update_user_password, name='update_user_password'),
+    path('forgot_password', views.forgot_password, name='forgot_password'),
+    path('reset_password/<str:token>', views.reset_password, name='reset_password')
     ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
+from django.utils.crypto import get_random_string
 from .serializers import SignUpSerializer, UserSerializer
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from datetime import timedelta, datetime
+from django.core.mail import send_mail
+from django.utils import timezone
 
 
 @api_view(['POST'])
@@ -61,6 +66,9 @@ def update_user_info(request):
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def update_user_password(request):
+     """
+        update the user password
+     """
      user = request.user
      data = request.data 
 
@@ -71,3 +79,67 @@ def update_user_password(request):
      serializer = UserSerializer(user, many=False)
 
      return Response(serializer.data)
+
+
+def get_current_host(request):
+    """
+        get the current host name
+    """
+    protocol = request.is_secure() and 'https' or 'http'
+    host = request.get_host()
+    return "{protocol}://{host}/".format(protocol=protocol,host=host)
+
+
+
+@api_view(['POST'])
+def forgot_password(request):
+    """
+        send a password reset link to registered user email to change password.
+    """
+    data = request.data
+    user = get_object_or_404(User, email=data['email'])
+
+    token = get_random_string(40)
+    expire_date=datetime.now() + timedelta(minutes=30)
+
+    user.userprofile.reset_password_token=token
+    user.userprofile.reset_password_expire=expire_date
+
+    user.userprofile.save()
+    host= get_current_host(request)
+    print(host)
+
+    link = "{host}api/reset_password/{token}".format(host=host, token=token)
+    print(link)
+    body = "Your password reset link is: {link}".format(link=link)
+    subject="Password Reset link for EShop"
+    send_mail(subject, 
+              body, 
+              "noreply@eshop.com",
+              [data['email']]
+            )
+    return Response({'details':" Password reset email sent to {email}".format(email=data['email'])})
+
+
+
+@api_view(['POST'])
+def reset_password(request, token):
+    """
+    user recives an email to reset the password on registered email.
+    user needs to enter password and confirmPassword.
+    """
+    data = request.data 
+    user = get_object_or_404(User, userprofile__reset_password_token=token)
+    # for time zone related issues 
+    if user.userprofile.reset_password_expire.replace(tzinfo=None) < datetime.now():
+        return Response({"error": "Token has expired"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if data['password'] != data['confirmPassword']:
+        return Response({'error': "Passwords doesn't match!"}, status=status.HTTP_400_BAD_REQUEST)
+    user.password = make_password(data['password'])
+    user.userprofile.reset_password_token= "" 
+
+    user.userprofile.reset_password_expire = None 
+    user.userprofile.save()
+    user.save()
+    return Response({"details": "Password reset done successfully!"})
